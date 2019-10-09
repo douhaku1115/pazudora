c80c43449c7a07f2c48ac45c117d00f9e3a62126
douhaku <douhaku1115@gmail.com>
2019-10-09 22:54:02 +0900

    整列ブロック消し

--- Source.cpp
+++ Source.cpp
@@ -35,6 +35,7 @@ char cellAA[][2 + 1] = {
 
 };
 int cursorX, cursorY;
+bool locked = false;
 int getConnectedBlockCount(int _x, int _y, int _cellType, int _count) {
 	if (
 		(_x<0 || _x>FIELD_WIDTH || _y<0 || _y>FIELD_HEIGHT)
@@ -51,54 +52,96 @@ int getConnectedBlockCount(int _x, int _y, int _cellType, int _count) {
 	_count = getConnectedBlockCount(_x+1, _y, _cellType, _count);
 
 }
+void eraseConnectedBlocks(int _x, int _y, int _cellType) {
+	if (
+		(_x<0 || _x>FIELD_WIDTH || _y<0 || _y>FIELD_HEIGHT)
+		|| cells[_y][_x] == CELL_TYPE_NONE
+		|| cells[_y][_x] != _cellType
+		)
+		return ;
+
+	cells[_y][_x] = CELL_TYPE_NONE;
+	eraseConnectedBlocks(_x, _y-1, _cellType);
+	eraseConnectedBlocks(_x-1, _y, _cellType);
+	eraseConnectedBlocks(_x, _y+1, _cellType);
+	eraseConnectedBlocks(_x+1 ,_y, _cellType);
+}
+void display() {
+	system("cls");
+	for (int y = 0; y < FIELD_HEIGHT; y++) {
+		for (int x = 0; x < FIELD_WIDTH; x++)
+			if ((x == cursorX) && (y == cursorY)&&(!locked))
+				printf("");
+			else
+				printf("%s", cellAA[cells[y][x]]);
+		if (y == selectedY)
+			printf("©");
+		printf("\n");
+	}
+
+	for (int x = 0; x < FIELD_WIDTH; x++)
+		printf("%s", (x == selectedX) ? "ª" : "@");
+}
 int main() {
 	srand((unsigned int)time(NULL));
 	for (int y = 0; y < FIELD_HEIGHT; y++)
 		for (int x = 0; x < FIELD_WIDTH; x++)
 			cells[y][x] = CELL_TYPE_BLOCK_0 + rand() % BLOCK_TYPE_MAX;
 
+	
+	time_t t = time(NULL);
 	while (1) {
-		system("cls");
-		for (int y = 0; y < FIELD_HEIGHT; y++) {
-			for (int x = 0; x < FIELD_WIDTH; x++)
-				if ((x == cursorX) && (y == cursorY))
-					printf("");
-				else
-					printf("%s", cellAA[cells[y][x]]);
-			if (y == selectedY)
-				printf("©");
-			printf("\n");
-		}
-		for (int x = 0; x < FIELD_WIDTH; x++)
-			printf("%s", (x == selectedX) ? "ª" : "@");
-
-		switch (_getch()) {
-		case'w':cursorY--; break;
-		case's':cursorY++; break;
-		case'a':cursorX--; break;
-		case'd':cursorX++; break;
-		default:
-			if (selectedX < 0) {
-				selectedX = cursorX;
-				selectedY = cursorY;
-			}
-			else {
-				int temp = cells[cursorY][cursorX];
-				cells[cursorY][cursorX] = cells[selectedY][selectedX];
-				cells[selectedY][selectedX] = temp;
-				memset(checked, 0, sizeof(checked));
-
-				for (int y = 0; y < FIELD_HEIGHT; y++) 
-					for (int x = 0; x < FIELD_WIDTH; x++) {
-						int n = getConnectedBlockCount(x, y, cells[y][x], 0);
-						if (n >= 3)
+		if (t < time(NULL)) {
+			t = time(NULL);
+			if (locked) {
+				locked = false;
+				for (int y = FIELD_HEIGHT - 2; y >= 0; y--)
+					for (int x = 0; x < FIELD_WIDTH; x++)
+						if ((cells[y][x] != CELL_TYPE_NONE) &&
+							(cells[y + 1][x] == CELL_TYPE_NONE)) {
+							cells[y + 1][x] = cells[y][x];
 							cells[y][x] = CELL_TYPE_NONE;
-					}
-
-
-				selectedX = selectedY = -1;
+							locked = true;
+						}
 			}
-			break;
+			display();
+			
+		}
+		if (_kbhit()) {
+			if (locked)
+				_getch();
+			else {
+				switch (_getch()) {
+				case'w':cursorY--; break;
+				case's':cursorY++; break;
+				case'a':cursorX--; break;
+				case'd':cursorX++; break;
+				default:
+					if (selectedX < 0) {
+						selectedX = cursorX;
+						selectedY = cursorY;
+					}
+					else {
+						int temp = cells[cursorY][cursorX];
+						cells[cursorY][cursorX] = cells[selectedY][selectedX];
+						cells[selectedY][selectedX] = temp;
+						memset(checked, 0, sizeof(checked));
+
+						for (int y = 0; y < FIELD_HEIGHT; y++)
+							for (int x = 0; x < FIELD_WIDTH; x++) {
+								int n = getConnectedBlockCount(x, y, cells[y][x], 0);
+								if (n >= 3)
+									eraseConnectedBlocks(x, y, cells[y][x]);
+							}
+
+
+						selectedX = selectedY = -1;
+						locked = true;
+					}
+					break;
+				}
+				display();
+			}
 		}
 	}
 }

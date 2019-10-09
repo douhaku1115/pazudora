d3c834ec0701d8d112ff970782cc7256406a3e0d
douhaku <douhaku1115@gmail.com>
2019-10-09 19:23:51 +0900

    連結数　計算

--- Source.cpp
+++ Source.cpp
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <time.h>
 #include <conio.h>
 #define FIELD_WIDTH 8
@@ -18,6 +19,7 @@ enum {
 	};
 
 int cells[FIELD_HEIGHT][FIELD_WIDTH];
+int checked[FIELD_HEIGHT][FIELD_WIDTH];
 int selectedX=-1, selectedY=-1;
 
 char cellAA[][2 + 1] = {
@@ -33,7 +35,22 @@ char cellAA[][2 + 1] = {
 
 };
 int cursorX, cursorY;
+int getConnectedBlockCount(int _x, int _y, int _cellType, int _count) {
+	if (
+		(_x<0 || _x>FIELD_WIDTH || _y<0 || _y>FIELD_HEIGHT)
+		|| checked[_y][_x]
+		||cells[_y][_x]==CELL_TYPE_NONE
+		|| cells[_y][_x] !=_cellType
+		)
+		return _count;
+	_count++;
+	checked[_y][_x] = true;
+	_count=getConnectedBlockCount(_x, _y-1, _cellType, _count);
+	_count = getConnectedBlockCount(_x-1, _y, _cellType, _count);
+	_count = getConnectedBlockCount(_x, _y+1, _cellType, _count);
+	_count = getConnectedBlockCount(_x+1, _y, _cellType, _count);
 
+}
 int main() {
 	srand((unsigned int)time(NULL));
 	for (int y = 0; y < FIELD_HEIGHT; y++)
@@ -69,6 +86,16 @@ int main() {
 				int temp = cells[cursorY][cursorX];
 				cells[cursorY][cursorX] = cells[selectedY][selectedX];
 				cells[selectedY][selectedX] = temp;
+				memset(checked, 0, sizeof(checked));
+
+				for (int y = 0; y < FIELD_HEIGHT; y++) 
+					for (int x = 0; x < FIELD_WIDTH; x++) {
+						int n = getConnectedBlockCount(x, y, cells[y][x], 0);
+						if (n >= 3)
+							cells[y][x] = CELL_TYPE_NONE;
+					}
+
+
 				selectedX = selectedY = -1;
 			}
 			break;
